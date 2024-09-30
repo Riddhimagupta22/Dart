6504027269d858c4770cf46354405e9d7145b07e
Riddhima Gupta <griddhima226@gmail.com>
2024-09-30 19:09:53 +0530

    Add files via upload

---
+++ simple calculator.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print('Enter the value of a');
+  double a = double.parse(stdin.readLineSync()!);
+  print('Enter the operator ');
+  String operator = stdin.readLineSync()!;
+  print('Enter the value of b');
+  double b = double.parse(stdin.readLineSync()!);
+
+  if (operator == "+") {
+    print(a + b);
+  } else if (operator == "-") {
+    print(a - b);
+  } else if (operator == "*") {
+    print(a * b);
+  } else if (operator == "/") {
+    print(a / b);
+  } else {
+    print("invalid");
+  }
+}
